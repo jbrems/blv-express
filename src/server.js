bf665613af0396332142f61bd97d62d77492e854
Jonas Brems <jbrems@devenv.local>
2021-12-23 21:35:29 +0100

    Simple express server setup

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Hello from the Blaasveld.net express server!');
+});
+
+const port = process.env.PORT || 3000;
+
+console.log(`Starting server on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
