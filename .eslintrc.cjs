6681cd233d01f80cd63d42610deecb809fd0cc42
Jonas Brems <jbrems@devenv.local>
2021-12-24 10:40:31 +0100

    Add eslint and refactor to ESM

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  rules: {
+    semi: ['error', 'always']
+  }
+};
