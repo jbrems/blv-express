0b6976cca24d3da55ca25864baad38d57e58dc84
Jonas Brems <jbrems@devenv.local>
2021-12-24 12:46:43 +0100

    Add basic error handling and content-type header to tile controller

--- src/tile/tile.controller.js
+++ src/tile/tile.controller.js
@@ -2,7 +2,15 @@ import { fetchTile } from './tile.service';
 
 export async function getTile (req, res) {
   const { z, x, y } = req.params;
-  console.log(`Getting tile for ${z} ${x} ${y}`);
-  const tileStream = await fetchTile(z, x, y);
-  tileStream.pipe(res);
+  
+  try {
+    console.log(`Getting tile for ${z} ${x} ${y}`);
+    const tileStream = await fetchTile(z, x, y);
+
+    res.set('Content-Type', 'image/png');
+    tileStream.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 }
--- src/tile/tile.service.js
+++ src/tile/tile.service.js
@@ -4,5 +4,8 @@ export async function fetchTile (z, x, y) {
   const tileUrl = `https://tile.openstreetmap.org/${z}/${x}/${y}.png`;
   console.log(`Fetching tile from ${tileUrl}`);
   const response = await fetch(tileUrl);
+
+  if (!response.ok) throw new Error(`Failed to fetch tile from ${tileUrl}`);
+
   return response.body;
 }
